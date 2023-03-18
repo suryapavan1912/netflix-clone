3a85f21ad3fc6cb349251c6242f857eca97b0940
suryapavan1912 <pavansurya1912@gmail.com>
2023-03-18 11:25:03 +0530

    firebase auth

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXkZezt69VfOYacdYQS6hfmfSmNoz-fTQ",
-  authDomain: "netflix-f7c7c.firebaseapp.com",
-  projectId: "netflix-f7c7c",
-  storageBucket: "netflix-f7c7c.appspot.com",
-  messagingSenderId: "328930609356",
-  appId: "1:328930609356:web:8c4901ef8b5751c5d39a69"
+  apiKey: "AIzaSyBM32V7-HNktpL36q-qzNGJW3TSmFdmHEY",
+  authDomain: "netflix-f7c7.firebaseapp.com",
+  projectId: "netflix-f7c7",
+  storageBucket: "netflix-f7c7.appspot.com",
+  messagingSenderId: "758516134101",
+  appId: "1:758516134101:web:e1b0b32411ec76fcd3f25c"
 };
 
 const app = initializeApp(firebaseConfig);
